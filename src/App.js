a1d97400e0c30aeaefa14fa99c79b8966e1e87aa
aermakov27 <aermakov27082002@gmail.com>
2021-12-20 02:10:29 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react  from "react";
+import Home from "./Home/Home";
+import HomeItems from "./Home/HomeItems";
+
+function App() {
+  return (
+    <div className="global">
+      <Home/>
+      <HomeItems/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/HomeItems.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function HomeItems() {
+
+    return(
+
+        <div>
+          <h2>Текст</h2>
+        </div>
+    )
+}
